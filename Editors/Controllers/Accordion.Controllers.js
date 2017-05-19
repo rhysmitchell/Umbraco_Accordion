2570eecb91428aa42181bfc480955b99d586f034
Rhys.Mitchell <Rhys.Mitchell@ceredigion.gov.uk>
2017-05-19 09:59:17 +0100

    initial commit

---
+++ Editors/Controllers/Accordion.Controllers.js
@@ -0,0 +1,33 @@
+﻿(function () {
+
+    "use strict";
+
+    function Controller($scope) {
+        var viewModel = this;
+        viewModel.openOverlay = openOverlay;
+
+        if (!$scope.control.sections) {
+                $scope.control.sections = [];
+            }
+
+        function openOverlay() {
+            viewModel.overlay = {
+                title: "Accordion",
+                view: "/App_Plugins/Accordion/Editors/Views/Overlay.html",
+                editModel: $scope.control.sections,
+                show: true,
+                submit: function (model) {
+                    viewModel.overlay.show = false;
+                    viewModel.overlay = null;
+                },
+                close: function (oldModel) {
+                    viewModel.overlay.show = false;
+                    viewModel.overlay = null;
+                }
+            }
+
+        };
+    }
+
+    angular.module("umbraco").controller("Accordion.Controller", Controller);
+})();
---
+++ Editors/Controllers/AccordionOverlay.Controllers.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    "use strict";
+
+    function Controller($scope) {
+        $scope.addNewSection = function (model, section) {
+            // Push new accordion item
+            model.editModel.push({
+                title: "",
+                content: ""
+            });
+
+            // Make Rich Text Editor of each content property
+            angular.forEach(model.editModel, function (value, key) {
+                value.content = {
+                    label: 'bodyText',
+                    description: 'Load some stuff here',
+                    view: 'rte',
+                    value: value.content.value,
+                    config: {
+                        editor: {
+                            toolbar: ["code", "undo", "redo", "cut", "styleselect", "bold", "italic", "alignleft", "aligncenter", "alignright", "bullist", "numlist", "link", "umbmediapicker", "umbmacro", "table", "umbembeddialog"],
+                            stylesheets: [],
+                            dimensions: { width: 380, height: 300 }
+                        }
+                    }
+                };
+            });
+
+            // Finally, set the new model
+            $scope.model.editModel = model.editModel;
+        };
+
+
+        $scope.removeSection = function (model, section) {
+            var index = model.editModel.indexOf(section);
+            model.editModel.splice(index, 1);
+        };
+    }
+
+    angular.module("umbraco").controller("AccordionOverlay.Controller", Controller);
+})();
